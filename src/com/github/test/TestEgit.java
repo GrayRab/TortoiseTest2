3f050bb66906f90322aed0d510d0b557ddf3bd1c
GrayRab <xuxu_mimi@163.com>
2016-05-27 23:53:41 +0800

    github update 01

--- src/com/github/test/TestEgit.java
+++ src/com/github/test/TestEgit.java
@@ -3,6 +3,8 @@ package com.github.test;
 public class TestEgit {
 	public static void main(String[] args) {
 		int a = 1;
+		
+		System.out.println("AAAAAAAAAAAA");
 		System.out.println("github update 01");
 		double price = 20.0;
 		System.out.println("local update 01");
