b4e7321680d9f7446485d27f639aaf5aa48abf47
GrayRab <xuxu_mimi@163.com>
2016-05-27 23:27:49 +0800

    sdfv

--- src/com/github/test/TestEgit.java
+++ src/com/github/test/TestEgit.java
@@ -4,9 +4,9 @@ public class TestEgit {
 	public static void main(String[] args) {
 		int a = 1;
 		System.out.println("github update 01");
+		double price = 20.0;
 		System.out.println("local update 01");
 		String gg = "dddd";
-		System.out.println("asdghak");
 		String flag = "abc";
 		System.out.println("asaadgad");
 	}
