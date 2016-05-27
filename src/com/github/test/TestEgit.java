4bbf4ef4e225ada4696f78bd56e8c388610683de
GrayRab <xuxu_mimi@163.com>
2016-05-27 22:37:29 +0800

    local update 02

--- src/com/github/test/TestEgit.java
+++ src/com/github/test/TestEgit.java
@@ -2,7 +2,7 @@ package com.github.test;
 
 public class TestEgit {
 	public static void main(String[] args) {
-		
+		int a = 1;
 		System.out.println("github update 01");
 		System.out.println("local update 01");
 	}
